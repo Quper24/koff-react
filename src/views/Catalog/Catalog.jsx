ce6eeb2b595acd3b6391ff8d29278789e8eef9f8
Maksim Leskin <maskim.leskin@methed.ru>
2024-01-20 16:28:00 +0300

    day6

--- src/views/Catalog/Catalog.jsx
+++ src/views/Catalog/Catalog.jsx
@@ -3,6 +3,7 @@ import s from './Catalog.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCategories } from '../../store/categories/categories.slice.js';
+import { Link } from 'react-router-dom';
 
 export const Catalog = () => {
   const dispatch = useDispatch();
@@ -22,9 +23,9 @@ export const Catalog = () => {
         <ul className={s.list}>
           {data.map((item, i) => (
             <li key={i}>
-              <a className={s.link} href={`/category?slug=${item}`}>
+              <Link className={s.link} to={`/category?category=${item}`}>
                 {item}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
